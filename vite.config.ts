108c28f454338a17e22db592461a47a6c649f35d
Nolan Picard <nolan.picard@iut-dijon.u-bourgogne.fr>
2025-11-28 00:53:46 +0100

    remove twitch entry from rollup input configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        twitch: resolve(__dirname, "twitch.html"),
       },
     },
   },
